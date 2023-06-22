da4dfce9c6ea562e67d0c57bd7477c241c7ba0a3
maximusblvck <masixolentissa@gmail.com>
2023-06-21 17:07:04 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -  a program that prints the alphabet in lowercase, followed by a new line
+ * return: 0 (successful)
+ */
+int main(void)
+{
+	char c;
+	c = 'a';
+	while
+	(c <= 'a')
+	{
+	putchar(c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
