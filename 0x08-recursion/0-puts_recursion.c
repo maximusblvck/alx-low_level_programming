d0d9e7d54bc472109202f5b5851f1d415b99d6b1
maximusblvck <masixolentissa@gmail.com>
2023-07-06 13:55:36 -0700

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -15,11 +15,13 @@ void _puts_recursion(char *s)
 
 /**
  * main - Entry point of the program.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
 	char *str = "Hello, World!";
+
 	_puts_recursion(str);
 
 	return (0);
