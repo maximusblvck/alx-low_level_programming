b55120a725dd93894177b6cfdfa247e7c10b32c9
maximusblvck <masixolentissa@gmail.com>
2023-07-06 13:46:13 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - Prints a string recursively.
+ * @s: Pointer to the string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	putchar(*s);
+	_puts_recursion(s + 1);
+}
+
+/**
+ * main - Entry point of the program.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str = "Hello, World!";
+	_puts_recursion(str);
+
+	return (0);
+}
